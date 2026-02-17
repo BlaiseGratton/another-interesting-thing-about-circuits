c5fa9ad760185406e1b9f5baac5dec757be7fd21
Blaise Gratton <blaisegratton@gmail.com>
2026-02-17 11:00:36 -0600

    better XOR layout

--- js/gates/xor-gate.js
+++ js/gates/xor-gate.js
@@ -8,71 +8,71 @@ export class XorGate extends ComponentContainer {
   }
 
   connectedCallback() {
-    this.width = 75
-    this.height = 125
+    this.width = 125
+    this.height = 75
     this.setAttribute('leftPorts', this.leftPorts)
     this.setAttribute('rightports', this.rightPorts)
     super.connectedCallback()
 
     if (this.svg) {
       this.orGate = document.createElement('or-gate')
-      this.orGate.setAttribute('scale', 0.35)
-      this.orGate.x = 18
+      this.orGate.setAttribute('scale', 0.28)
+      this.orGate.x = 21
       this.orGate.y = 1
       this.appendChild(this.orGate)
 
       this.inputWire1 = document.createElement('wire-element')
       this.inputWire1.x1 = 8.5
-      this.inputWire1.y1 = 40
-      this.inputWire1.x2 = 10
-      this.inputWire1.y2 = 17
+      this.inputWire1.y1 = 23.5
+      this.inputWire1.x2 = 12.5
+      this.inputWire1.y2 = 13
       this.appendChild(this.inputWire1)
 
       this.inputWire2 = document.createElement('wire-element')
       this.inputWire2.x1 = 8.5
-      this.inputWire2.y1 = 43
-      this.inputWire2.x2 = 10
-      this.inputWire2.y2 = 94
+      this.inputWire2.y1 = 26
+      this.inputWire2.x2 = 12.5
+      this.inputWire2.y2 = 50
       this.appendChild(this.inputWire2)
 
       this.inputWire3 = document.createElement('wire-element')
       this.inputWire3.x1 = 8.5
-      this.inputWire3.y1 = 82
-      this.inputWire3.x2 = 10
-      this.inputWire3.y2 = 30
+      this.inputWire3.y1 = 49
+      this.inputWire3.x2 = 12.5
+      this.inputWire3.y2 = 25
       this.appendChild(this.inputWire3)
 
       this.inputWire4 = document.createElement('wire-element')
       this.inputWire4.x1 = 8.5
-      this.inputWire4.y1 = 85
-      this.inputWire4.x2 = 10
-      this.inputWire4.y2 = 109
+      this.inputWire4.y1 = 51
+      this.inputWire4.x2 = 12.5
+      this.inputWire4.y2 = 61
       this.appendChild(this.inputWire4)
 
       this.nandGate = document.createElement('nand-gate')
-      this.nandGate.setAttribute('scale', 0.35)
-      this.nandGate.x = 18
-      this.nandGate.y = 80
+      this.nandGate.setAttribute('scale', 0.28)
+      this.nandGate.x = 21
+      this.nandGate.y = 38
       this.appendChild(this.nandGate)
 
       this.andGate = document.createElement('and-gate')
-      this.andGate.setAttribute('scale', 0.23)
-      this.andGate.x = 42
-      this.andGate.y = 48
+      this.andGate.setAttribute('scale', 0.5)
+      this.andGate.x = 71.5
+      this.andGate.y = 6
       this.appendChild(this.andGate)
 
       this.orOutputWire = document.createElement('wire-element')
-      this.orOutputWire.x1 = 52
-      this.orOutputWire.y1 = 23
-      this.orOutputWire.x2 = 34
-      this.orOutputWire.y2 = 58
+      this.orOutputWire.x1 = 50
+      this.orOutputWire.y1 = 19
+      this.orOutputWire.x2 = 63
+      this.orOutputWire.y2 = 27
       this.appendChild(this.orOutputWire)
 
       this.nandOutputWire = document.createElement('wire-element')
-      this.nandOutputWire.x1 = 52
-      this.nandOutputWire.y1 = 102
-      this.nandOutputWire.x2 = 34
-      this.nandOutputWire.y2 = 67
+      this.nandOutputWire.x1 = 50
+      this.nandOutputWire.y1 = 55
+      this.nandOutputWire.x2 = 63
+      this.nandOutputWire.y2 = 48
       this.appendChild(this.nandOutputWire)
     }
   }
