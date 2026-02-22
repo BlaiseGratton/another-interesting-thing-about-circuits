32ced1432cb3d45de720eac5ce24d8f1c2ee003f
Blaise Gratton <blaisegratton@gmail.com>
2026-02-22 12:59:21 -0600

    implement vertical NOR-gate

--- js/gates/nor-gate.js
+++ js/gates/nor-gate.js
@@ -9,51 +9,49 @@ export class NorGate extends ComponentContainer {
 
   connectedCallback() {
     this.width = 75
-    this.height = 125
-    this.setAttribute('leftports', this.leftPorts)
-    this.setAttribute('rightports', this.rightPorts)
-    super.connectedCallback()
+    this.height = 75
 
-    if (this.svg) {
-      this.switch1 = document.createElement('double-throw-relay')
-      this.switch1.setAttribute('scale', 0.2)
-      this.switch1.x = 16
-      this.switch1.y = 29
-      this.appendChild(this.switch1)
+    if (this.isHorizontal) {
+      this.setAttribute('leftports', this.leftPorts)
+      this.setAttribute('rightports', this.rightPorts)
+      super.connectedCallback()
 
-      this.wire1 = document.createElement('wire-element')
-      this.wire1.x1 = 64
-      this.wire1.y1 = 41
-      this.wire1.x2 = 64
-      this.wire1.y2 = 56
-      this.appendChild(this.wire1)
+      if (this.svg) {
+        this.inverter1 = this.addComponent('double-throw-relay', 16, 4, {
+          scale: 0.2,
+          orientation: 'horizontal'
+        })
+        this.inverter2 = this.addComponent('inverter-relay', 16, 36, {
+          scale: 0.2,
+          orientation: 'horizontal'
+        })
+        this.wire1 = this.addWire(8, 24, 8, 16)
+        this.wire2 = this.addWire(64, 16, 64, 31)
+        this.wire3 = this.addWire(64, 31, 8, 31)
+        this.wire4 = this.addWire(8, 31, 8, 42)
+        this.wire5 = this.addWire(64, 47, 67, 37)
+      }
+    } else {
+      this.topPorts = 2
+      this.bottomPorts = 1
+      this.setAttribute('topports', this.topPorts)
+      this.setAttribute('bottomports', this.bottomPorts)
 
-      this.switch2 = document.createElement('inverter-switch')
-      this.switch2.setAttribute('scale', 0.2)
-      this.switch2.x = 16
-      this.switch2.y = 66.5
-      this.appendChild(this.switch2)
+      super.connectedCallback()
 
-      this.wire2 = document.createElement('wire-element')
-      this.wire2.x1 = 64
-      this.wire2.y1 = 56
-      this.wire2.x2 = 8
-      this.wire2.y2 = 62
-      this.appendChild(this.wire2)
-
-      this.wire3 = document.createElement('wire-element')
-      this.wire3.x1 = 8
-      this.wire3.y1 = 62
-      this.wire3.x2 = 8
-      this.wire3.y2 = 75
-      this.appendChild(this.wire3)
-
-      this.wire4 = document.createElement('wire-element')
-      this.wire4.x1 = 64
-      this.wire4.y1 = 79
-      this.wire4.x2 = 67
-      this.wire4.y2 = 63
-      this.appendChild(this.wire4)
+      if (this.svg) {
+        this.inverter1 = this.addComponent('double-throw-relay', 4, 16, {
+          scale: 0.2
+        })
+        this.inverter2 = this.addComponent('inverter-relay', 36, 16, {
+          scale: 0.2
+        })
+        this.wire1 = this.addWire(24, 8, 16, 8)
+        this.wire2 = this.addWire(16, 64, 31, 64)
+        this.wire3 = this.addWire(31, 64, 31, 8)
+        this.wire4 = this.addWire(31, 8, 42, 8)
+        this.wire5 = this.addWire(47, 64, 37, 67)
+      }
     }
   }
 }
